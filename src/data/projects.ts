7bcd6b8f56308ec977858a1c61a275a6ac2391c5
cel3ntano <adriano2024celentano@gmail.com>
2025-02-27 23:15:00 +0100

    chore: update projects list

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,12 +2,21 @@ import { Project } from '@/types/project';
 
 export const projects: Project[] = [
   {
+    id: 'lexiGrow',
+    title: 'Lexi Grow',
+    description:
+      'A vocabulary-building app built with Next.js 15, designed to help Ukrainian speakers learn English words. The app features a personal dictionary where users can add, categorize, and track words, a recommendation system to discover new vocabulary, and an interactive training module for practice. Built with a responsive design using Tailwind CSS, it includes user authentication, advanced filtering capabilities, and progress tracking.',
+    githubLink: 'https://github.com/cel3ntano/lexi_grow',
+    liveLink: 'https://lexigrow.zhygalko.dev/',
+    technologies: ['Next.js', 'Typescript', 'Redux', 'Tailwind CSS'],
+  },
+  {
     id: 'linguaBridge',
     title: 'Lingua Bridge',
     description:
       'A modern web application built with Next.js and TypeScript that helps users find language tutors. Users can browse teachers, filter by language, level, and price, view detailed profiles with reviews, and book trial lessons. The app features Firebase authentication, allowing users to create accounts and maintain a favorites list. Built with a responsive design, it works seamlessly across devices and uses Tailwind CSS for styling and Redux Toolkit for state management.',
     githubLink: 'https://github.com/cel3ntano/LinguaBridge',
-    liveLink: 'https://linguabridge.zhygalko.dev//',
+    liveLink: 'https://linguabridge.zhygalko.dev/',
     technologies: ['Next.js', 'Typescript', 'Tailwind CSS'],
   },
   {
@@ -18,14 +27,6 @@ export const projects: Project[] = [
     githubLink: 'https://github.com/cel3ntano/campers',
     liveLink: 'https://campers.zhygalko.dev/',
     technologies: ['React', 'Redux'],
-  },
-  {
-    id: 'taskpro',
-    title: 'TaskPro App',
-    description: `A team project aimed to help with task organization using boards, columns, and cards, inspired by Trello. My main contribution was to develop the frontend for user authentication, ensuring smooth integration with the backend API. I used Redux for state management, which improved the app's performance and made it easier to maintain. This project helped me enhance my skills in frontend development, state management, and teamwork.`,
-    githubLink: 'https://github.com/maksymPr424/TaskPro',
-    liveLink: 'https://www.task-pro.app/',
-    technologies: ['React', 'Redux', 'Node.js'],
   },
   {
     id: 'portfolio',
@@ -48,11 +49,19 @@ export const projects: Project[] = [
     id: 'project_vegetables_24',
     title: 'Green Harvest',
     description: `A team project to create a store concept for a vegetable delivery service. 
-The project included features like easy navigation, product listings, user reviews, a service description, and a contact form. As the team lead, I took the responsibility of reviewing and integrating all sections to ensure a cohesive design. This early project was a great learning opportunity, significantly improving my HTML and CSS skills while also helping me develop teamwork and collaboration abilities.`,
+    The project included features like easy navigation, product listings, user reviews, a service description, and a contact form. As the team lead, I took the responsibility of reviewing and integrating all sections to ensure a cohesive design. This early project was a great learning opportunity, significantly improving my HTML and CSS skills while also helping me develop teamwork and collaboration abilities.`,
     githubLink: 'https://github.com/cel3ntano/project-vegetables24',
     liveLink: 'https://projects.zhygalko.dev/project-vegetables24/',
     technologies: ['HTML', 'CSS', 'JavaScript'],
   },
+  // {
+  //   id: 'taskpro',
+  //   title: 'TaskPro App',
+  //   description: `A team project aimed to help with task organization using boards, columns, and cards, inspired by Trello. My main contribution was to develop the frontend for user authentication, ensuring smooth integration with the backend API. I used Redux for state management, which improved the app's performance and made it easier to maintain. This project helped me enhance my skills in frontend development, state management, and teamwork.`,
+  //   githubLink: 'https://github.com/maksymPr424/TaskPro',
+  //   liveLink: 'https://www.task-pro.app/',
+  //   technologies: ['React', 'Redux', 'Node.js'],
+  // },
   // {
   //   id: 'web_studio',
   //   title: 'Web Studio',
